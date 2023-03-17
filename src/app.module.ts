8af9c4d91a66c98cf0b93ebae0808455be37d166
a.krahel <artur.krahel@outlook.com>
2023-03-17 13:25:22 +0300

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import * as process from 'process';
+import { Dialect } from 'sequelize/types/sequelize';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { UsersModule } from './users/users.module';
+import configuration from './config/app';
 import { Users } from './users/users.model';
+import { UsersModule } from './users/users.module';
 
 @Module({
+  controllers: [AppController],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.development.env', '.local.env'],
+      load: [configuration],
+    }),
     SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'postgres',
+      database: process.env.DB_DATABASE,
+      dialect: process.env.DB_DIALECT as Dialect,
+      host: process.env.DB_HOST,
       models: [Users],
+      password: process.env.DB_PASSWORD,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
     }),
     UsersModule,
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
