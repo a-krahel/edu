b856fb37763ac7d001d18f8536f369c95fb1e918
a.krahel <artur.krahel@outlook.com>
2023-03-17 13:07:06 +0300

    feat: change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,16 @@
+/* eslint-disable */
 module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint', 'simple-import-sort', 'sort-keys-fix'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    'no-console': [
+      'error',
+      {
+        allow: ['error'],
+      },
+    ],
+    'simple-import-sort/imports': 'error',
+    'sort-keys-fix/sort-keys-fix': 'error',
   },
 };
