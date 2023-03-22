c95bee6ba5a8eb66eea491b48d37a9726c88652f
a.krahel <artur.krahel@outlook.com>
2023-03-22 14:52:32 +0300

    feat: update validate method

--- src/users/jwt.stategy.ts
+++ src/users/jwt.stategy.ts
@@ -1,5 +1,4 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectModel } from '@nestjs/sequelize';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -12,7 +11,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectModel(Users)
     private readonly usersModel: typeof Users,
-    private jwtService: JwtService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -21,12 +19,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(authorization) {
-    const payload = this.jwtService.decode(authorization.slice(7));
-    const email = payload['email'];
-
     const user = await this.usersModel.findOne({
       where: {
-        email,
+        email: authorization['email'],
       },
     });
 
